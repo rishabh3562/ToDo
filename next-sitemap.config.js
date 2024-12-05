7a0b793c1cb9575e47236f815c195925f788b745
rishabh3562 <rishabh.dubey.cs21@ggits.net>
2024-12-05 21:04:16 +0530

    sitemap changes

---
+++ next-sitemap.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  siteUrl: "https://neumotask.vercel.app", // Update to production URL
+  generateRobotsTxt: true, // (optional)
+  sitemapSize: 7000,
+  outDir: "./public", 
+  changefreq: "daily", 
+  priority: 0.7, 
+};
